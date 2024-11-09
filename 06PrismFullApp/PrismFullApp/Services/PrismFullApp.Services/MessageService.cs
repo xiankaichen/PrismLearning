0ca7ba166f583b7985d3d95a4e52718614696287
xiankai.chen@qq.com <xiankai.chen@qq.com>
2024-11-09 22:05:43 +0800

    添加了Prism框架下完整的App框架架构

---
+++ 06PrismFullApp/PrismFullApp/Services/PrismFullApp.Services/MessageService.cs
@@ -0,0 +1,12 @@
+﻿using PrismFullApp.Services.Interfaces;
+
+namespace PrismFullApp.Services
+{
+    public class MessageService : IMessageService
+    {
+        public string GetMessage()
+        {
+            return "你好，这是来自消息服务的问候！";
+        }
+    }
+}
